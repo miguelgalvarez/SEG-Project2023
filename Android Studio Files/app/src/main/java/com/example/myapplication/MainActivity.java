53aa29934ebb77c8c632f5590c7adbd9a522563a
miguelgalvarez <malva018@uottawa.ca>
2023-10-10 19:28:10 -0400

    Updating Database

--- Android Studio Files/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android Studio Files/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.net.Uri;
+import android.widget.Toast;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
