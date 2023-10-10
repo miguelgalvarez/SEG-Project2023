633e18ccb1597eb1321605d1144000f5a1388bc5
Zach-sikka <zachary.sikka@gmail.com>
2023-10-10 19:07:02 -0400

    adding email validation

--- Android Studio Files/app/src/main/java/com/example/myapplication/CreatingAccountActivity.java
+++ Android Studio Files/app/src/main/java/com/example/myapplication/CreatingAccountActivity.java
@@ -1,8 +1,18 @@
 package com.example.myapplication;
 
+import static android.graphics.Color.rgb;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.net.Uri;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import android.util.Patterns;
 
 public class CreatingAccountActivity extends AppCompatActivity {
 
@@ -10,5 +20,27 @@ public class CreatingAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creating_account);
+    }
+
+    public boolean validate(View view){
+        EditText email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        if (!email.getText().toString().isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()){
+            return true;
+        }
+        return false;
+    }
+
+    public void CreateAccount(View view) {
+
+        EditText email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        if (!email.getText().toString().isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()){
+            Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+            startActivityForResult(intent, 0);
+        } else {
+            email.setHint("Please enter a password");
+            email.setHintTextColor(rgb(255,0,0));
+        }
+
+
     }
 }
