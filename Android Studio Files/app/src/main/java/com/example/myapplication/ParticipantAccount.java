4f58ff0ec7ea1ee490dc6cb7d59e59abe2e67201
Zach-sikka <zachary.sikka@gmail.com>
2023-10-13 13:25:21 -0400

    adding everything

---
+++ Android Studio Files/app/src/main/java/com/example/myapplication/ParticipantAccount.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+public class ParticipantAccount extends Account{
+
+    public ParticipantAccount(String username, String password, String firstName, String lastName, String email, String accountType) {
+
+        super(username, password, firstName, lastName, email, accountType);
+    }
+}
