bb7534fed9b22246738cfd6d7ca3b72b9d792aea
Zach-sikka <zachary.sikka@gmail.com>
2023-10-15 21:30:16 -0400

    cleaning up code and adding javaDoc

--- Android Studio Files/app/src/main/java/com/example/myapplication/EventType.java
+++ Android Studio Files/app/src/main/java/com/example/myapplication/EventType.java
@@ -1,5 +1,11 @@
 package com.example.myapplication;
 
+/**
+ * Enumeration class for the 5 types of events
+ *
+ * @author Zachary Sikka
+ * @version 1.0
+ */
 public enum EventType {
     TIME_TRIAL("Time Trial"),
 
@@ -19,11 +25,21 @@ public enum EventType {
 
     }
 
+    /**
+     * Getter method for returning the event type
+     *
+     * @return the String representation of the event type
+     */
     public String getName() {
 
         return this.name;
     }
 
+    /**
+     * Getter method for returning the event type
+     *
+     * @return the String representation of the event type
+     */
     @Override
     public String toString() {
 
--- Android Studio Files/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android Studio Files/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,6 +13,12 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+/**
+ * Main Activity that display the main login/signup screen for our cycling app
+ *
+ * @author Android Studio, Zachary Sikka, Zachary Kranabetter, Migeul Alvarez
+ * @version 1.0
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,13 +26,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    /**
+     * Method that represents the action when pressing the login button
+     *
+     * @param view gives information about the UI components
+     */
     public void AdminLogin(View view) {
 
         Account newAccount = new AdministratorAccount("admin", "admin", "ADMIN", "ADMIN", "ADMIN@admin.com", AccountType.ADMINISTRATOR);
         EditText adminUsername = (EditText) findViewById(R.id.usernameHint);
         EditText adminPassword = (EditText) findViewById(R.id.passwordHint);
 
-        //Application Context and Activity
         if (adminPassword.getText().toString().equals("admin") && adminUsername.getText().toString().equals("admin")) {
 
             Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
@@ -37,20 +49,29 @@ public class MainActivity extends AppCompatActivity {
 
         } else {
 
+            //error message that displays at the bottom of the screen
             Toast toast = Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT);
             toast.show();
         }
 
+
         if (adminUsername.getText().toString().isEmpty()) {
+
             adminUsername.setError("Please enter a username");
 
         }
 
         if (adminPassword.getText().toString().isEmpty()) {
+
             adminPassword.setError("Please enter a password");
         }
     }
 
+    /**
+     * Method that represents the action when pressing the Create Account button
+     *
+     * @param view gives information about the UI components
+     */
     public void CreateAccountLink(View view) {
         //Application Context and Activity
         Intent intent = new Intent(getApplicationContext(), CreateAccountActivity.class);
