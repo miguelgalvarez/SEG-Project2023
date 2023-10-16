12d7c940c2eff39ab7750ed10d7cdf754db9d24a
Zach-sikka <zachary.sikka@gmail.com>
2023-10-15 20:23:35 -0400

    adding the skeleton methods for the admin functionality

--- Android Studio Files/app/src/main/java/com/example/myapplication/AdministratorAccount.java
+++ Android Studio Files/app/src/main/java/com/example/myapplication/AdministratorAccount.java
@@ -1,8 +1,26 @@
 package com.example.myapplication;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class AdministratorAccount extends Account{
 
     public AdministratorAccount(String username, String password, String firstName, String lastName, String email, AccountType accountType) {
+
         super(username, password, firstName, lastName, email, accountType);
+    }
+
+    public void addEventType() {
+
+        //hash map storing the events that can be offered and their required fields
+        HashMap<EventType, ArrayList> eventsOffered = new HashMap<>();
+
+        ArrayList<String> requiredFields = new ArrayList<>();
+
+    }
+
+
+    public Account removeAccount() {
+        return null;
     }
 }
