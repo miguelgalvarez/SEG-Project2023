43106c1c440fae92eec56b64d13f842568fc68c9
lindensheehy <linden.sheehy@outlook.com>
2023-11-10 14:02:23 -0500

    finished event list ui

--- Android Studio Files/app/src/main/java/com/example/myapplication/EventTypeList.java
+++ Android Studio Files/app/src/main/java/com/example/myapplication/EventTypeList.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -54,15 +56,37 @@ public class EventTypeList extends ArrayAdapter<EventType> {
         listViewItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Apply slide-in animation to the edit button
-                Animation slideIn = AnimationUtils.loadAnimation(getContext(), R.anim.slide_in);
-                viewHolder.editButton.startAnimation(slideIn);
-                viewHolder.editButton.setVisibility(View.VISIBLE);
 
-                // Apply slide-in animation to the delete button
-                Animation slideInDelete = AnimationUtils.loadAnimation(getContext(), R.anim.slide_in);
-                viewHolder.deleteButton.startAnimation(slideInDelete);
-                viewHolder.deleteButton.setVisibility(View.VISIBLE);
+                if (viewHolder.buttonsVisible) {
+                    // Apply slide-in animation to the edit button
+                    Animation slideOut = AnimationUtils.loadAnimation(getContext(), R.anim.slide_out);
+
+                    viewHolder.editButton.startAnimation(slideOut);
+                    viewHolder.editButton.setVisibility(View.INVISIBLE);
+
+                    viewHolder.deleteButton.startAnimation(slideOut);
+                    viewHolder.deleteButton.setVisibility(View.INVISIBLE);
+
+                    viewHolder.arrow.startAnimation(slideOut);
+                    viewHolder.arrow.setVisibility(View.INVISIBLE);
+
+                    viewHolder.buttonsVisible = false;
+                }
+
+                else {
+                    // Apply slide-in animation to the edit button
+                    Animation slideIn = AnimationUtils.loadAnimation(getContext(), R.anim.slide_in);
+                    viewHolder.editButton.startAnimation(slideIn);
+                    viewHolder.editButton.setVisibility(View.VISIBLE);
+
+                    viewHolder.deleteButton.startAnimation(slideIn);
+                    viewHolder.deleteButton.setVisibility(View.VISIBLE);
+
+                    viewHolder.arrow.startAnimation(slideIn);
+                    viewHolder.arrow.setVisibility(View.VISIBLE);
+
+                    viewHolder.buttonsVisible = true;
+                }
             }
         });
 
@@ -74,11 +98,18 @@ public class EventTypeList extends ArrayAdapter<EventType> {
         TextView eventTypeName;
         Button editButton;
         Button deleteButton;
+        ImageView threeDots;
+        ImageView arrow;
+        Boolean buttonsVisible;
+
 
         public ViewHolder(View itemView) {
             eventTypeName = itemView.findViewById(R.id.textViewName);
             editButton = itemView.findViewById(R.id.editButton);
             deleteButton = itemView.findViewById(R.id.deleteButton);
+            threeDots = itemView.findViewById(R.id.threeDots);
+            arrow = itemView.findViewById(R.id.arrow);
+            buttonsVisible = false;
         }
     }
 }
